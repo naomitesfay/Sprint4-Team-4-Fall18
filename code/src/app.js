aca80c34601f49d07a210fc0e7b21176217d354b
Sofia Berglund <berglund.sofia@gmail.com>
2018-10-17 14:56:56 +0200

    fetch api on clubs-page

--- code/src/app.js
+++ code/src/app.js
@@ -1,11 +1,12 @@
 import React from "react"
+import ClubsPage from "./pages/clubs-page"
 
 class App extends React.Component {
 
   render() {
     return (
       <div>
-        Find me in src/app.js!
+        <ClubsPage />
       </div>
     )
   }
--- code/src/pages/clubs-page.js
+++ code/src/pages/clubs-page.js
@@ -2,14 +2,39 @@ import React from "react"
 
 class ClubsPage extends React.Component {
 
-  render() {
-    return (
-      <div>
-        ClubsPage
-      </div>
-    )
+  state = {
+  allClubs: [],
+  search: []
+}
+
+componentDidMount() {
+  fetch(`https://api.www.svenskaspel.se/player/sponsorship/autocomplete?search=${this.state.search}&numResponses=100`).then((response) => {
+    return response.json()
+  }).then((json) => {
+    console.log(json)
+  })
+}
+
+handleSearch = (event) => {
+    this.setState({
+      search: event.target.value
+    })
   }
 
-}
+  render() {
+
+    // if (this.state.allClubs.length > 0) {
+      return (
+        <div className="search-field">
+          <form>
+            <input type="text" onChange={this.handleSearch}/>
+          </form>
+
+        </div>
+      )
+    }
+  }
+
+
 
 export default ClubsPage
