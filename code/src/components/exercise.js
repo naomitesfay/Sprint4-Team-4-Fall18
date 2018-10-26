6fc36f16f597d6728268fc82d8e99234ea645746
Sofia Berglund <berglund.sofia@gmail.com>
2018-10-26 12:59:04 +0200

    changes

--- code/src/components/exercise.js
+++ code/src/components/exercise.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "./exercise.scss"
+
 class Exercise extends React.Component {
 
   constructor(props) {
@@ -54,21 +55,21 @@ class Exercise extends React.Component {
         <div className="exerciseForm">
           <div className="buttonBox">
             <h2>Easy: 1 point</h2>
-            <button onClick={this.handleIncrementOne}>Taking the stairs</button>
-            <button onClick={this.handleIncrementOne}>Taking a walk</button>
+            <button onClick={this.handleIncrementOne}>taking the stairs</button>
+            <button onClick={this.handleIncrementOne}>taking a walk</button>
             <button onClick={this.handleIncrementOne}>One hour standing at work</button>
           </div>
           <div className="buttonBox">
             <h2>Medium: 2 points</h2>
-            <button onClick={this.handleIncrementTwo}>Biking to work</button>
-            <button onClick={this.handleIncrementTwo}>Planking in the office 2 min</button>
+            <button onClick={this.handleIncrementTwo}>biking to work</button>
+            <button onClick={this.handleIncrementTwo}>Planking in the office 2min</button>
             <button onClick={this.handleIncrementTwo}>25 squats</button>
           </div>
           <div className="buttonBox">
             <h2>Hard: 3 points</h2>
             <button onClick={this.handleIncrementThree}>Going to the gym</button>
             <button onClick={this.handleIncrementThree}>Spinning</button>
-            <button onClick={this.handleIncrementThree}>Static wallsit for 5 min</button>
+            <button onClick={this.handleIncrementThree}>Static wallsit for 5min</button>
           </div>
         </div>
         <div className="counter">
