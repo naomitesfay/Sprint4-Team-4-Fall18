c9131e40018909af70cf9b5c4261d38d6b5a5b6c
Sofia Berglund <berglund.sofia@gmail.com>
2018-10-26 13:24:44 +0200

    added images

--- code/src/components/exercise.js
+++ code/src/components/exercise.js
@@ -55,18 +55,27 @@ class Exercise extends React.Component {
         <div className="exerciseForm">
           <div className="buttonBox">
             <h2>Easy: 1 point</h2>
+            <div className="exercise-image">
+              <img src="images/1point.png" />
+            </div>
             <button onClick={this.handleIncrementOne}>taking the stairs</button>
             <button onClick={this.handleIncrementOne}>taking a walk</button>
             <button onClick={this.handleIncrementOne}>One hour standing at work</button>
           </div>
           <div className="buttonBox">
             <h2>Medium: 2 points</h2>
+            <div className="exercise-image">
+              <img src="images/2points.png" />
+            </div>
             <button onClick={this.handleIncrementTwo}>biking to work</button>
             <button onClick={this.handleIncrementTwo}>Planking in the office 2min</button>
             <button onClick={this.handleIncrementTwo}>25 squats</button>
           </div>
           <div className="buttonBox">
             <h2>Hard: 3 points</h2>
+            <div className="exercise-image">
+              <img src="images/3points.png" />
+            </div>
             <button onClick={this.handleIncrementThree}>Going to the gym</button>
             <button onClick={this.handleIncrementThree}>Spinning</button>
             <button onClick={this.handleIncrementThree}>Static wallsit for 5min</button>
